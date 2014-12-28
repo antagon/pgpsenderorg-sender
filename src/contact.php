157de953e9183471aa703a0fb9ce4ff20144f039
antagon <antagon@codeward.org>
2014-12-28 13:51:31 +0100

    Contact information and privacy policy.

---
+++ src/contact.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Copyright (c) 2013 - 2015, PGPSender.org
+ */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Contact - PGPSender</title>
+<?php
+	require_once ("include/head.php");
+?>
+	</head>
+	<body>
+		<div id="content">
+			<h1> <a href="/">PGPSender</a> </h1>
+			<div id="quote">
+<?php
+	require_once ("include/quote.php");
+?>
+			</div>
+
+			<h2>Contact</h2>
+
+			<ul>
+				<li> <strong>Email:</strong> info@pgpsender.org</li>
+				<li> <strong>Twitter:</strong> <a href="https://twitter.com/PGPSender">@PGPSender</a> </li>
+			</ul>
+
+			<ul>
+				<ol></ol>
+			</ul>
+		</div>
+		
+		<div id="footer">
+<?php
+	require_once ("include/footer.php");
+?>
+		</div>
+	</body>
+</html>
+
---
+++ src/privacy.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright (c) 2013 - 2015, PGPSender.org
+ */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Privacy policy - PGPSender</title>
+<?php
+	require_once ("include/head.php");
+?>
+	</head>
+	<body>
+		<div id="content">
+			<h1> <a href="/">PGPSender</a> </h1>
+			<div id="quote">
+<?php
+	require_once ("include/quote.php");
+?>
+			</div>
+
+			<h2>Privacy</h2>
+
+			<p>This document lays down a key tenets of our privacy policy in respect to user and user's data.</p>
+
+			<ol>
+				<li>We <strong>shall not</strong> expose email addresses to anyone.</li>
+				<li>We <strong>shall not</strong> preserve email addresses longer than is neccessary for the delivery.</li>
+				<li>We <strong>shall not</strong> prowl through unencrypted content of the emails.</li>
+				<li>We <strong>shall not</strong> preserve content of the emails.</li>
+				<li>We <strong>shall not</strong> send unsolicited emails.</li>
+				<li>We <strong>shall</strong> reveal publicly all attempts to break our vow to these tenets.</li>
+			</ol>
+
+			<p>We reserve a right to log website access and build visit statistics, including browser information and IP address.</p>
+		</div>
+		
+		<div id="footer">
+<?php
+	require_once ("include/footer.php");
+?>
+		</div>
+	</body>
+</html>
+
