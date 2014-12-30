264e7c6ac6931360e44914d508cf4e79b408b6f8
antagon <antagon@codeward.org>
2014-12-30 23:53:11 +0100

    Pass object instead of string.
    Character '+' was stripped from data, let jQuery take care of
    the escaping so no stripping occurs.

--- src/js/pgpsender_api.js
+++ src/js/pgpsender_api.js
@@ -2,7 +2,8 @@ function PGPSender ()
 {
 	this.email_send = function (access_token, recipient, sender, subject, message, public_key, callback)
 	{
-		$.post ("api/email_send.php", "access_token="+access_token+"&recipient="+recipient+"&sender="+sender+"&subject="+subject+"&message="+message+"&public_key="+public_key, function (response){
+		//$.post ("api/email_send.php", "access_token="+access_token+"&recipient="+recipient+"&sender="+sender+"&subject="+subject+"&message="+message+"&public_key="+encodeURIComponent (public_key), function (response){
+		$.post ("api/email_send.php", { "access_token": access_token, "recipient": recipient, "sender": sender, "subject": subject, "message": message, "public_key": public_key }, function (response){
 			var data = null;
 
 			try {
@@ -17,7 +18,8 @@ function PGPSender ()
 
 	this.email_get_pubkey = function (access_token, recipient, sender, callback)
 	{
-		$.post ("api/email_get_pubkey.php", "access_token="+access_token+"&recipient="+recipient+"&sender="+sender, function (response){
+		//$.post ("api/email_get_pubkey.php", "access_token="+access_token+"&recipient="+recipient+"&sender="+sender, function (response){
+		$.post ("api/email_get_pubkey.php", { "access_token": access_token, "recipient": recipient, "sender": sender }, function (response){
 			var data = null;
 
 			try {
